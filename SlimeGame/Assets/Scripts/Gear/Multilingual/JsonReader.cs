4565f5ced90000784420e531c0b3a7dce9e4833b
KenGsama <945456973@qq.com>
2021-01-31 15:10:18 +0800

    门——开关 预制体

---
+++ SlimeGame/Assets/Scripts/Gear/Multilingual/JsonReader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+public class JsonReader
+{
+    
+    public void Read(WordsData data)
+    {
+        
+        JsonUtility.FromJsonOverwrite(data.file.text, data);
+    }
+}
